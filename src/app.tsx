f96de976687466f270cbee845c9e6f0c63ca9a18
Bruno Ponsoni Costa <42554984+psonybruno@users.noreply.github.com>
2024-02-07 11:28:22 -0300

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,28 @@
+import logo from './assets/logo-nlw-expert.svg';
+import { NoteCard } from './components/note-card';
+
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt='NLW Expert' />
+      <form className="w-full">
+        <input type="text" placeholder='Busque suas notas....' className="w-full bg-transparent text-3xl  font-semibold tracking-tight placeholder:text-slate-500 outline-none" />
+      </form>
+
+      <div className='h-px bg-slate-700' />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <div className='rounded-md bg-slate-700 p-5 space-y-3'>
+          <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
+          <p className="text-sm leading-6 text-slate-400">Grave uma nota em áudio que será convertida para texto automaticamente.</p>
+        </div>
+
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+
+        
+      </div>
+    </div>
+  )
+}
