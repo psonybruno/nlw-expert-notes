5ec99e1c3bb5ed925b918f467e95484d87efa1c7
Bruno Ponsoni Costa <42554984+psonybruno@users.noreply.github.com>
2024-02-08 14:11:25 -0300

    Aula 03

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -4,15 +4,17 @@ import { ptBR } from 'date-fns/locale';
 import { X } from 'lucide-react';
 interface NoteCardProps {
   note: {
+    id: string
     date: Date
-    content: string
-  }
+    content: string   
+  },
+  onNoteDeleted: (id: string) => void
 }
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-700 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
-      <span className="text-sm font-medium text-slate-300">{note.date.toISOString()}</span>
+      <span className="text-sm font-medium text-slate-300">{formatDistanceToNow(note.date.toISOString(), { locale: ptBR, addSuffix: true})}</span>
       <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
       <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
@@ -20,13 +22,16 @@ export function NoteCard({ note }: NoteCardProps) {
     
       <Dialog.Portal>
         <Dialog.Overlay className='inset-0 fixed bg-black/50' />
-        <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+        <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2  md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none'>
           <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'><X className='size-5' /></Dialog.Close>
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">{formatDistanceToNow(note.date.toISOString(), { locale: ptBR, addSuffix: true})}</span>
             <p className="text-sm leading-6 text-slate-400">{note.content}</p>
           </div>
-          <button type='button' className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'>Deseja <span className='text-red-400 group-hover:underline '>apagar essa nota</span>?</button>
+          <button 
+            type='button' 
+            onClick={() => onNoteDeleted(note.id)}
+            className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'>Deseja <span className='text-red-400 group-hover:underline '>apagar essa nota</span>?</button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
